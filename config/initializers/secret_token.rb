73bd5a482f2ae73531d2ca30ea25ba31b8b4f955
sakairyota <sakai@cloudstudy.com>
2012-02-20 00:27:50 +0900

    rails new jsonp_example -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsonpExample::Application.config.secret_token = '5cda2abcf9d8a3986d2a8fbd16b2405d0d58de64dbcee48cbdfa0228cdcfe22037b5822d8209ddbe806adea8f6a7748207e84c3eb6625b828e6873e2860e6667'
