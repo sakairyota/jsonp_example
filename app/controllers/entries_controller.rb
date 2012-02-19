ca0e4a91d3f85196af10d67d719a8ca52cde6977
sakairyota <sakai@cloudstudy.com>
2012-02-20 01:01:27 +0900

    add callback to entries/show

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,7 +2,7 @@ class EntriesController < ApplicationController
   def show
     @entry = {id: params[:id], title: "entry#{params[:id]}", content: "This is content ##{params[:id]}"}
     respond_to do |format|
-      format.json { render json: @entry}
+      format.json { render json: @entry, callback: params[:callback]}
     end
   end
 end
